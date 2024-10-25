15b76978bc3477d337a7389e094e2450942960d4
AloBordo1992 <168308266+YuraShybalov@users.noreply.github.com>
2024-10-25 21:16:10 +0200

    add function delete

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+const itemTemplate = document.getElementById("todo-list-item-template");
+const listElement = document.getElementById("todo-list");
+const editForm = document.getElementById("edit-form");
+
+let id = 0;
+function* makeId() {
+  while (true) {
+    yield id++;
+  }
+}
+
+const idGenerator = makeId();
+
+editForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(editForm); //збираємо дані з форми
+  const data = Object.fromEntries(formData.entries());
+  let listItem;
+  if (editForm.dataset.mode === "create") {
+    if (data.title) {
+      listItem = itemTemplate.content.cloneNode(true);
+      listItem
+        .querySelector(".todo-item")
+        .setAttribute("id", `itemid-${idGenerator.next().value}`);
+      listItem.querySelector(".title").innerText = data.title;
+      listItem.querySelector(".desc").innerText = data.desc;
+      listElement.append(listItem);
+    }
+  } else if (editForm.dataset.mode === "edit") {
+    listItem = listElement.querySelector(`#${editForm.dataset.editId}`);
+    listItem.querySelector(".title").innerText = data.title;
+    listItem.querySelector(".desc").innerText = data.desc;
+  }
+  editForm.reset();
+  editForm.dataset.modeb = "create";
+  editForm.dataset.editId = "";
+});
+
+function editItem(element) {
+  editForm.dataset.mode = "edit";
+  editForm.dataset.editId = element.id;
+  editForm.querySelector("[name=title]").value =
+    element.querySelector(".title").innerText;
+  editForm.querySelector("[name=desc]").value =
+    element.querySelector(".desc").innerText;
+}
+
+function deleteItem(element) {
+  element.remove();
+}
+
+listElement.addEventListener("click", (event) => {
+  const item = event.target.closest(".todo-item");
+  switch (event.target.dataset.action) {
+    case "edit":
+      editItem(item);
+    case "delete":
+      deleteItem(item);
+    default:
+      return;
+  }
+});
